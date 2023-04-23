944c73e01b8a053ca79ea270a6c5fb5f9eb3cd67
Paula <paulamorenocor@outlook.com>
2023-04-23 18:17:57 +0200

    Added gitignore and doc

--- source/find_flight.py
+++ source/find_flight.py
@@ -8,14 +8,16 @@ import pandas as pd
 
 
 def search_flights(search:str):
+    # Primer paso es abrir el navegador con las opciones qu necesitemos, en este caso, incognito y sin extensiones
     options = webdriver.ChromeOptions()
     options.add_argument('--disable-extensions')
     options.add_argument('--incognito')
 
     driver = webdriver.Chrome(executable_path=r"./source/chromedrive_win32/chromedriver.exe",options=options)
+    # Una vez abierto el navegador, vamos a la página de google
     driver.get("https://www.google.com/search?q="+search)
     wait = WebDriverWait(driver, 10)
-
+    # Necesitamos saber en que idioma está la página para poder aceptar las cookies
     lang = driver.find_element(by=By.XPATH, value='//html')
     lang = lang.get_attribute('lang')
     if lang == 'es':
@@ -26,22 +28,29 @@ def search_flights(search:str):
         accept_cookies_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//div[contains(text(),"Accepta-ho tot")]/ancestor::button')))
     else:
         raise Exception("Language not supported")
-
+    # Una vez encontramos el botón para aceptar las cookies, lo pulsamos
     accept_cookies_button.click()
-
+    # El siguiente paso es acceder a la pagina del comparador de vuelos de google
     flight_btn = driver.find_element(by=By.XPATH, value='//g-more-link')
     flight_btn.click()
     wait.until(EC.presence_of_element_located((By.CLASS_NAME, "XsapA")))
+    # Por defecto google nos muestra los vuelos más baratos, pero si queremos ver los vuelos más rápidos, tenemos que pulsar el botón de "Más opciones"
     more_btn = driver.find_element(by=By.CLASS_NAME, value="XsapA")
     more_btn.click()
+    # Una vez pulsado el botón, tenemos que esperar a que se cargue la página y que se muestren las opciones. Luego ya podemos recuperar la lista de vuelos
     wait.until(EC.presence_of_element_located((By.XPATH, '//li')))
     time.sleep(10)
     flight_table = driver.find_elements(by=By.XPATH, value='//li')
+    # Creamos un dataframe para almacenar los datos de los vuelos
     dataset = pd.DataFrame(columns=['airline', 'price', 'duration', 'stops', 'departure', 'arrival','returns','datetime'])
+    # Guardamos la fecha y hora de la búsqueda
     date = datetime.datetime.now().isoformat()
+    # Recorremos la lista de vuelos y extraemos los datos que nos interesan
     for flight in flight_table: 
+        # Quitamos las recomendaciones de tren
         if 'Renfe' not in flight.text:
             aux = flight.text.split('\n')
+            # Descartamos los casos en los que no haya datos disponibles
             if len(aux)>1:
                 try:
                     departure = aux[0]
@@ -49,16 +58,18 @@ def search_flights(search:str):
                     airline = aux[3]
                     duration = aux[4]
                     stops = aux[6]
+                    # En algunos casos, el precio no está disponible, por lo que tenemos que tratarlo de forma diferente
                     if aux[-1]=="Precio no disponible":
                         price = aux[-1]
                         returns = "No disponible"
                     else:    
                         price = aux[-2]
                         returns = aux[-1]
+                    # Añadimos los datos a nuestro dataframe
                     dataset = dataset.append({'airline': airline, 'price': price, 'duration': duration, 'stops': stops, 'departure': departure, 'arrival': arrival,'returns':returns,'datetime':date},ignore_index=True)
                 except Exception as e:
                     print(str(e))
                     print(aux)
-    # Cerrar el navegador
+    # Cerramos el navegador
     driver.quit()
     return dataset
