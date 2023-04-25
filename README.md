5ab40fa2769aa8c70029252af25493d09c9c6b51
Paula <paulamorenocor@outlook.com>
2023-04-25 19:49:04 +0200

    Updated docs and readme

--- README.md
+++ README.md
@@ -12,3 +12,20 @@ David Cosialls Almeida
 
 ## Contenido del repositorio
 
+En este proyecto nos podemos encontrar los siguientes archivos:
+
+- README.md: archivo README del repositorio
+- requirements.txt: especificación de las librerias y las versiones que se utilizan en el proyecto
+- /dataset: carpeta donde se encuentra el archvio .csv
+- /dataset/flights.csv: archivo .csv con los datos resultantes de la busqueda
+- /source: carpeta con el código
+- /source/run.py: archivo principal que ejecuta el programa. 
+- /source/fetchWeb.py: archivo con la función para recuperar los destinos
+- /source/fetchSpainAiports.py: archivo con la función para recuperar los nombres de los aeropuertos
+- /source/find_flights.py: archivo con la función que realiza la busqueda de los vuelos
+- /source/spainTable.json: archivo con la correspondencia del codigo IATA con el aeropuerto
+
+
+## DOI
+
+- DOI de Zenodo: 10.5281/zenodo.7864245
--- source/run.py
+++ source/run.py
@@ -2,17 +2,22 @@ import pandas as pd
 from find_flight import search_flights
 from fetchWeb import fetch_airlines
 from fetchSpainAirports import get_airports
-from json import load
+#from json import load
 import os
 
 if __name__=="__main__":
+    #Recupera todos los destinos que se pueden volar desde Madrid
     destinos = fetch_airlines()
+    #Recupera todos los aeropuertos de España con su codigo IATA
     aeropuertos = get_airports()
+    #Crea un dataset con todos los vuelos desde Madrid a los destinos
     dataset = pd.DataFrame(columns=['airline', 'price', 'duration', 'stops', 'departure', 'arrival','returns','datetime','from','to'])
     for dest in destinos:
         if dest in aeropuertos:
+            #Genera la busqueda en google flights
             busqueda = 'vuelos madrid '+aeropuertos[dest].replace('Airport','')
             dataset = dataset.append(search_flights(busqueda),ignore_index=True)
+    #Si no esta creado el archivo lo crea, si no lo añade al final
     if not os.path.isfile('./dataset/flights.csv'):
         dataset.to_csv('./dataset/flights.csv',index=False)
     else: 
